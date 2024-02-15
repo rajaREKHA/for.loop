793a7d81d9c8edcd63441e089779ac0e91cda726
rajaREKHA <150239480+rajaREKHA@users.noreply.github.com>
2024-02-15 15:49:06 +0530

    Add files via upload

---
+++ rekha.js
@@ -0,0 +1,132 @@
+
+// a=prompt(" enter the no")
+// b=a%4
+// switch(b){
+//     case 0:
+//         document.write("remainder is 0")
+//         break
+//         case 1 :
+//             document.write("remainder is 1")
+//             break
+//             case 2:
+//                 document.write("remainder is 2")
+//                 break
+//                 case 3:
+//                     document.write("remainder is 3")
+//                     breakdefault:
+//                     document.write("enter valid no.")
+// }
+
+
+ 
+
+// day=parseInt(prompt("inter your case"))
+// switch(day){
+//     case 1:
+//     document.write("monday")
+//     break
+
+//     case 2:
+//         document.write(" tuesday")
+//         break
+//         case 3:
+//             document.write(" wednesday")
+//             break
+//             case 4:
+//                 document.write("thursday")
+//                 break
+//                 case 5:
+//                     document.write("friday")
+//                     break
+//                     case 6:
+//                         document.write(" saturday")
+//                         break
+//                         case 7:
+//                             document.write(" sunday")
+// default:
+//     document.write(" enter vailid no")
+// }
+// a=frompt(" enter fruit color")
+// switch(a){
+//     case " apple":
+//     document.write(" red")
+//     break
+// case" greaps":
+// document.write(" green")
+// break
+
+// case" orange":
+// document.write("orange")
+// break
+
+// case" banana":
+// document.write(" yellow")
+// break
+
+// case" papaya":
+// document .write("yellow")
+// break
+
+// default:
+//     document.write(" enter valid no")
+// }
+
+// 1=prompt(" enter manth name")
+// switch(1){
+//     case"1":
+//     document.write("january")
+//     break
+
+//     case"2":
+//     document.write("february")
+//     break
+    
+//     case"3":
+//     document.write("march")
+//     break
+//     case"4":
+//     document.write("april")
+//     break
+//     case"5":
+//     document.write("may")
+//     break
+//     case"6":
+//     document.write("june")
+//     break
+//     case"7":
+//     document.write("july")
+//     break
+//     case"8":
+//     document.write("august")
+//     break
+//     case"9":
+//     document.write("september")
+//     break
+//     case"10":
+//     document.write("october")
+//     break
+//     case"11":
+//     document.write("november")
+//     break
+//     case"12":
+//     document.write("december")
+//     default:
+//     document.write(" enter valid no.")
+    
+// }
+
+// for(a=50;a<=100;++a){
+//     document.write(a,"<br>")
+// }
+
+
+for(b=1;b<=100;++b){
+    if(b%2==0){
+        document.write(b,"is evenno.<br>")
+    }
+    alse{
+        document.write(b,"is odd no.<br>")
+    }
+}
+
+
